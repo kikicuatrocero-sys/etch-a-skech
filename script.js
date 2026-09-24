c8b77c0f8fffb32bef180d1d103216002046982a
Cristhian <kikicuatrocero@gmail.com>
2026-09-24 10:57:57 -0500

    Improved the createGrid() function by refining the grid structure—using a `div` for every 16 squares before starting a new column—and added classes and styles to each created "div" so the squares are visible.

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 //Inicializamos las variables a usar
 const container = document.querySelector("div");
+/*
+createGrid (size)
+size = Tamaño del grid size x size
+Itera desde 0 hasta el tamaño o size, por cada iteracion, 
+crea una column con la clase column
+despues iteramos con otro for para crear 16 grids por cada columna
+con la clase grid.
+Las añadimos  al column padre y el column la añadimos al container padre
+*/
 function createGrid(size) {
-  size = size * 2;
-  console.log(size)
+  size = size;
   for (let i = 0; i < size; i++) {
-    let div = document.createElement("div");
-    container.appendChild(div);
+    let column = document.createElement("div");
+    column.classList.add("column");
+    container.appendChild(column);
+    for (let j = 0; j < size; j++) {
+      let div = document.createElement("div");
+      div.classList.add("grid");
+      column.appendChild(div);
+    }
   }
-  console.log("Completado");
 }
+//Iniciamos con un tamaño de 16x16
 createGrid(16);
