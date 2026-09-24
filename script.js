a845da640ee653f44a16434fece6d00ceb7f7e55
Cristhian <kikicuatrocero@gmail.com>
2026-09-24 11:43:10 -0500

    We create the reset function, prompt for input, clear the container, and call the `createGrid` function to generate a new grid with squares sized according to the reset function's parameter.

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
 //Inicializamos las variables a usar
 const container = document.querySelector("div");
+const resetButton = document.querySelector("button");
+
+/*
+Funcion reset
+Pedimos por prompt un valor
+Comprobamos que no cancele (cuando cancela, el prompt devuelve null)
+Despues convertirmos a Number, el size tiene que ser entero
+Cuando escribimos un string, el Number() devuelve NaN y no un integer
+entonces comprobramos que no sea NaN para salir del bucle
+Con container, su innerHTML (hasta el contenido como de texto ademas del contenido de sus hijos)
+los convertimos en null o string vacio eliminando a sus hijos
+llamamos despues a la funcion de crear el grid pasandole el parametro en integer de size
+ */
+function reset() {
+  let size;
+  do {
+    size = prompt("Escribe el tamaño");
+    if (size === null) return null;
+    size = Number(size);
+  } while (isNaN(size));
+  container.innerHTML = "";
+  createGrid(size);
+}
+resetButton.addEventListener("click", reset);
+
 /*
 createGrid (size)
 size = Tamaño del grid size x size
@@ -19,6 +44,9 @@ function createGrid(size) {
       let div = document.createElement("div");
       div.classList.add("grid");
       column.appendChild(div);
+      div.addEventListener("mouseenter", () => {
+        div.style.backgroundColor = "black";
+      });
     }
   }
 }
